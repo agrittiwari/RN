efcda83396dcb9c6c6ee828c238bef8f576ab14c
Agrit Tiwari <agrit1408@gmail.com>
2021-01-04 18:15:01 +0530

    UserInput form accepting user input

--- AddContactForm.js
+++ AddContactForm.js
@@ -23,13 +23,26 @@ export default class AddContactForm extends Component {
       phone: '',
   }
   
+handleNameChange = name => {
+    this.setState({name})
+}
 
 
+handlePhoneChange = phone => {
+    this.setState({phone})
+}
     render() {
         return (
       <View style={ {paddingTop:20}}>
-        <TextInput style={styles.input}     value ={this.state.name} />
-        <TextInput  style={styles.input} value={this.state.phone}/>
+        <TextInput 
+        style={styles.input} 
+        onChangeText={this.handleNameChange} 
+        value ={this.state.name} 
+        />
+        <TextInput 
+         style={styles.input}
+         onChangeText={this.handlePhoneChange}
+         value={this.state.phone}/>
         
         <Button title="Add Contact"/>  
     </View>
